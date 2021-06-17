12036f5a396f38d0d42d275244c1ac3131148e89
Pranjal Sahu <pranjalsahu5@gmail.com>
2021-06-17 13:56:03 -0400

    Adding the individual cmake build example for ImageRegistration1

---
+++ TestCMakeExample/bin/CMakeFiles/ImageRegistration1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImageRegistration1.dir/ImageRegistration1.cxx.o"
+  "ImageRegistration1"
+  "ImageRegistration1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImageRegistration1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
